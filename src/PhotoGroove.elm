162ab2d5e310c065edc5f686d620539e4d679b4e
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2022-04-04 09:10:45 +0200

    Start adding fetching from server

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -13,20 +13,40 @@ urlPrefix =
     "http://elm-in-action.com/"
 
 
+type Msg
+    = GotSelectedIndex Int
+    | ClickedPhoto String
+    | ClickedSize ThumbnailSize
+    | ClickedSurpriseMe
+
+
 view : Model -> Html Msg
 view model =
-    div [ class "content" ]
-        [ h1 [] [ text "Photo Groove" ]
-        , button [ onClick ClickedSurpriseMe ] [ text "Surprise Me!" ]
-        , h3 [] [ text "Thumbnail Size:" ]
-        , div [ id "choose-size" ] (List.map viewSizeChooser [ Small, Medium, Large ])
-        , div [ id "thumbnails", class (sizeToString model.chosenSize) ] (List.map (viewThumbnail model.selectedUrl) model.photos)
-        , img
-            [ class "large"
-            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
-            ]
-            []
+    div [ class "content" ] <|
+        case model.status of
+            Loaded photos selectedUrl ->
+                viewLoaded photos selectedUrl model.chosenSize
+
+            Loading ->
+                []
+
+            Errored errorMessage ->
+                [ text ("Error:" ++ errorMessage) ]
+
+viewLoaded : List Photo -> String -> ThumbnailSize -> List (Html Msg)
+viewLoaded photos selectedUrl chosenSize =
+    [ h1 [] [ text "Photo Groove" ]
+    , button [ onClick ClickedSurpriseMe ] [ text "Surprise Me!" ]
+    , h3 [] [ text "Thumbnail Size:" ]
+    , div [ id "choose-size" ] (List.map viewSizeChooser [ Small, Medium, Large ])
+    , div [ id "thumbnails", class (sizeToString chosenSize) ]
+        (List.map (viewThumbnail selectedUrl) photos)
+    , img
+        [ class "large"
+        , src (urlPrefix ++ "large/" ++ selectedUrl)
         ]
+        []
+    ]
 
 
 viewThumbnail : String -> Photo -> Html Msg
@@ -70,32 +90,26 @@ type alias Photo =
     { url : String }
 
 
+type Status
+    = Loading
+    | Loaded (List Photo) String
+    | Errored String
+
+
 type alias Model =
-    { photos : List Photo, selectedUrl : String, chosenSize : ThumbnailSize }
+    { status : Status, chosenSize : ThumbnailSize }
+
+
+initialModel : Model
+initialModel =
+    { status = Loading
+    , chosenSize = Medium
+    }
 
 
 photoArray : Array Photo
 photoArray =
     Array.fromList initialModel.photos
-
-
-initialModel : Model
-initialModel =
-    { photos =
-        [ { url = "1.jpeg" }
-        , { url = "2.jpeg" }
-        , { url = "3.jpeg" }
-        ]
-    , selectedUrl = "1.jpeg"
-    , chosenSize = Medium
-    }
-
-
-type Msg
-    = GotSelectedIndex Int
-    | ClickedPhoto String
-    | ClickedSize ThumbnailSize
-    | ClickedSurpriseMe
 
 
 getPhotoUrl : Int -> String
@@ -117,20 +131,22 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GotSelectedIndex index ->
-            ( {model | selectedUrl = getPhotoUrl index}, Cmd.none)
+            ( { model | selectedUrl = getPhotoUrl index }, Cmd.none )
+
+        ClickedPhoto url ->
+            ( { model | selectedUrl = url }, Cmd.none )
+
         ClickedSize size ->
             ( { model | chosenSize = size }, Cmd.none )
 
         ClickedSurpriseMe ->
             ( model, Random.generate GotSelectedIndex randomPhotoPicker )
 
-        ClickedPhoto url ->
-            ( { model | selectedUrl = url }, Cmd.none )
 
 main : Program () Model Msg
 main =
     Browser.element
-        { init = \flags -> (initialModel, Cmd.none)
+        { init = \flags -> ( initialModel, Cmd.none )
         , view = view
         , update = update
         , subscriptions = \model -> Sub.none
