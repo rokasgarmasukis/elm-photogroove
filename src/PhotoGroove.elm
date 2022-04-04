b385d2661fd16ecdc81138a0ba90a374bf8dec74
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2022-04-04 07:18:46 +0200

    Add random generator

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -5,6 +5,7 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Random
 
 
 urlPrefix : String
@@ -86,12 +87,13 @@ initialModel =
         , { url = "3.jpeg" }
         ]
     , selectedUrl = "1.jpeg"
-    , chosenSize = Large
+    , chosenSize = Medium
     }
 
 
 type Msg
-    = ClickedPhoto String
+    = GotSelectedIndex Int
+    | ClickedPhoto String
     | ClickedSize ThumbnailSize
     | ClickedSurpriseMe
 
@@ -106,22 +108,30 @@ getPhotoUrl index =
             ""
 
 
-update : Msg -> Model -> Model
+randomPhotoPicker : Random.Generator Int
+randomPhotoPicker =
+    Random.int 0 (Array.length photoArray - 1)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ClickedSize size -> 
-            { model | chosenSize = size}
+        GotSelectedIndex index ->
+            ( {model | selectedUrl = getPhotoUrl index}, Cmd.none)
+        ClickedSize size ->
+            ( { model | chosenSize = size }, Cmd.none )
+
         ClickedSurpriseMe ->
-            { model | selectedUrl = "2.jpeg" }
+            ( model, Random.generate GotSelectedIndex randomPhotoPicker )
 
         ClickedPhoto url ->
-            { model | selectedUrl = url }
+            ( { model | selectedUrl = url }, Cmd.none )
 
-
-
+main : Program () Model Msg
 main =
-    Browser.sandbox
-        { init = initialModel
+    Browser.element
+        { init = \flags -> (initialModel, Cmd.none)
         , view = view
         , update = update
+        , subscriptions = \model -> Sub.none
         }
